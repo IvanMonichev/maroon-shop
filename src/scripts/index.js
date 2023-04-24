b5343afc8c7fc891ea8efd4a6912623005bdac82
Ivan Monichev <ivan@monichev.ru>
2023-04-24 19:19:25 +0300

    feat: add header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+import $ from 'jquery';
+
+$(document).ready(() => {
+  $('.header__menu-button').on('click', () => {
+    $('.header__menu-button').toggleClass('header__menu-button--active');
+    $('.root').toggleClass('root--lock');
+    $('.header').toggleClass('header--colored');
+    $('.navigation').fadeToggle().css('display', 'flex');
+  });
+});
