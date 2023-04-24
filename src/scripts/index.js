1d2cb3a3bdce1cde2af4b16d06e3bca555b2246c
Ivan Monichev <ivan@monichev.ru>
2023-04-24 21:00:09 +0300

    feat: add promotion section (WIP)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,22 @@
 import $ from 'jquery';
 
 $(document).ready(() => {
+
+  // Burger menu
   $('.header__menu-button').on('click', () => {
     $('.header__menu-button').toggleClass('header__menu-button--active');
     $('.root').toggleClass('root--lock');
     $('.header').toggleClass('header--colored');
     $('.navigation').fadeToggle().css('display', 'flex');
   });
+
+  // Scroll header
+
+  $(window).on('scroll', () => {
+    if ($(window).scrollTop() > 50) {
+      $('.header').addClass('header--colored');
+    } else {
+      $('.header').removeClass('header--colored');
+    }
+  })
 });
