0b69f9d13fc2e18bb5c092a486a00745a67fb2e6
Ivan Monichev <ivan@monichev.ru>
2023-04-26 21:16:58 +0300

    feat: complete promotion section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,6 @@ $(document).ready(() => {
   $('.header__menu-button').on('click', () => {
     $('.header__menu-button').toggleClass('header__menu-button--active');
     $('.root').toggleClass('root--lock');
-    $('.header').toggleClass('header--colored');
     $('.navigation').fadeToggle().css('display', 'flex');
   });
 
